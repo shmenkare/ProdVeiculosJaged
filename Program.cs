9dbf578781351efd26dcf9f058346e4d6c08fcd5
“shmenkare” <shmenkare@gmail.com>
2023-09-30 17:10:31 -0400

    Corrigiendo

--- Program.cs
+++ Program.cs
@@ -8,60 +8,66 @@ namespace ProdVeiculosJaged
     {
         static void Main(string[] args)
         {
-            int i = 0, j = 0, ventaDia = 0, totDias = 20, cantMay = 0, cantMen = 100;
+            int i = 0, j = 0, costoDia = 0, vehicprod = 0, totDias = 20, cantMay = 0, cantMen = 100;
             float sum = 1.0f, prom = 1.0f;
 
-            int[,] VentaSemanal = new int[4, 5];
+            int[][] CostoDiario = new int[5][];
 
-            for (i = 0; i < 4; i++)
+
+            for (i = 0; i < CostoDiario.Length; i++)
             {
-                for (j = 0; j < 5; j++)
+                Console.WriteLine("Cantidad de vehiculos producidos en el dia {0} ", i);
+                vehicprod = Convert.ToInt32(Console.ReadLine());
+                CostoDiario[i] = new int[vehicprod];
+
+                for (j = 0; j < CostoDiario[i].Length; j++)
                 {
-                    Console.WriteLine("Cantidad de vehiculos producidos en el dia {0}{1}", i, j);
-                    ventaDia = Convert.ToInt32(Console.ReadLine());
-                    VentaSemanal[i, j] = ventaDia;
-                    sum += ventaDia;
-                    Console.Clear();
+                    Console.WriteLine("Costo del vehiculo {1} producido en el dia {0} ", i, j);
+                    costoDia = Convert.ToInt32(Console.ReadLine());
+                    CostoDiario[i][j] = costoDia;
+                    sum += costoDia;
+                   
                 }
+                Console.Clear();
             }
             Console.WriteLine("Mostrando tabla");
-            for (i = 0; i < 4; i++)
+            for (i = 0; i < CostoDiario.Length; i++)
             {
-                for (j = 0; j < 5; j++)
+                for (j = 0; j < CostoDiario[i].Length; j++)
                 {
                     Thread.Sleep(100);
-                    Console.Write(VentaSemanal[i, j]);
+                    Console.Write(CostoDiario[i][j]);
 
                 }
                 Console.WriteLine();
             }
             prom = sum / totDias;
 
-            Console.WriteLine("El promedio de ventas es {0} ", prom);
-            for (i = 0; i < 4; i++)
+            Console.WriteLine("El promedio de costos es {0} ", prom);
+            for (i = 0; i < CostoDiario.Length; i++)
             {
-                for (j = 0; j < 5; j++)
+                for (j = 0; j < CostoDiario[i].Length; j++)
                 {
-                    if (VentaSemanal[i, j] < cantMen)
+                    if (CostoDiario[i][j] < cantMen)
                     {
-                        cantMen = VentaSemanal[i, j];
+                        cantMen = CostoDiario[i][j];
                     }
                 }
 
             }
-            for (i = 0; i < 4; i++)
+            for (i = 0; i < CostoDiario.Length; i++)
             {
-                for (j = 0; j < 5; j++)
+                for (j = 0; j < CostoDiario[i].Length; j++)
                 {
-                    if (VentaSemanal[i, j] > cantMay)
+                    if (CostoDiario[i][j] > cantMay)
                     {
-                        cantMay = VentaSemanal[i, j];
+                        cantMay = CostoDiario[i][j];
                     }
                 }
 
             }
-            Console.WriteLine("La menor cantidad producida es {0} ", cantMen);
-            Console.WriteLine("La mayor cantidad producida es {0} ", cantMay);
+            Console.WriteLine("La menor costo es {0} ", cantMen);
+            Console.WriteLine("La mayor costo es {0} ", cantMay);
             Console.ReadKey();
         }
 
